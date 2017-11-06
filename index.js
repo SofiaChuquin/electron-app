89a206ebf601c6d8b2d4a2d4de001b3bd0888226
Sofia Chuquin <sofiachuquin@gmail.com>
2017-11-06 10:44:22 -0500

    Fix issue with center mode

--- index.js
+++ index.js
@@ -16,9 +16,31 @@ $(document).ready(function(){
   });
 
   $('.photos').slick({
+    dots: true,
+    autoplay: true,
     centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   $(this).keydown((e) => {
